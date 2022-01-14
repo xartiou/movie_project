c6fa810484ce05d37e7e4b3b0a35a74eeba2d3aa
xartiou <xartiou@gmail.com>
2022-01-14 20:25:31 +0300

    Создал таблицу Movie

--- movie_app/models.py
+++ movie_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+# class - это таблица
+# атрибуты класса - это записи(колонки) в таблице
+# models.Model автоматом создает ID
+class Movie(models.Model):
+    name = models.CharField(max_length=40)
+    rating = models.IntegerField()
