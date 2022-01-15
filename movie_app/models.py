82ee4ea218bf65aa55f22126250a3a9e7b5adc77
xartiou <xartiou@gmail.com>
2022-01-15 15:43:02 +0300

    - создал функцию get_url в movie для вызова url по id - в шаблоне all_movies.html заменил в  ссылке на get_url

--- movie_app/models.py
+++ movie_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 # class - это таблица
@@ -9,6 +10,10 @@ class Movie(models.Model):
     rating = models.IntegerField()
     year = models.IntegerField(null=True)
     budget = models.IntegerField(default=10)
+
+    # функция вызова url по id
+    def get_url(self):
+        return reverse('movie_detail', args=[self.id])
 
     def __str__(self):
         return f'{self.name}  - {self.rating}%'
