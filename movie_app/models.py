1493467da70ccffe164fed1d3e227df0bbff4f4d
xartiou <xartiou@gmail.com>
2022-01-15 11:30:48 +0300

    - классу Movie добавил атрибуты year и budget - произвел makemigrations migrate

--- movie_app/models.py
+++ movie_app/models.py
@@ -7,6 +7,8 @@ from django.db import models
 class Movie(models.Model):
     name = models.CharField(max_length=40)
     rating = models.IntegerField()
+    year = models.IntegerField(null=True)
+    budget = models.IntegerField(default=10)
 
     def __str__(self):
         return f'{self.name} - {self.rating}%'
