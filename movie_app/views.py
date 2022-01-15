3198f2b370a61252ee19903d94caa494b5f52cf9
xartiou <xartiou@gmail.com>
2022-01-15 20:39:46 +0300

    примеры aggregate

--- movie_app/views.py
+++ movie_app/views.py
@@ -1,14 +1,21 @@
 from django.shortcuts import render, get_object_or_404
 from  .models import Movie
 from django.db.models import F
+# функции агрегации
+from django.db.models import Sum, Avg, Max, Min, Count
+
 # Create your views here.
+
 
 def show_all_movies(request):
     movies = Movie.objects.order_by(F('year').desc(nulls_last=True))  # или .asc или nulls_first=True
+    agg = movies.aggregate(Avg("budget"), Min("rating"), Max("rating"))
     for movie in movies:
         movie.save()
     context = {
-        'movies': movies
+        'movies': movies,
+        'agg': agg,
+        'count_mov': movies.count()
     }
     return render(request, 'movie_app/all_movies.html', context=context)
 
